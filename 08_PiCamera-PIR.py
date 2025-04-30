5d1289bec8768a0fce49145b184f7aba36a7be71
prerit-deakin <preritjaiswal65@gmail.com>
2025-04-30 15:43:10 +0530

    first commit

---
+++ 08_PiCamera-PIR.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as gpio
+from picamera import PiCamera
+import time
+pir=23
+i=0
+gpio.setmode(gpio.BCM)
+gpio.setup(pir,gpio.IN)
+
+while True:
+	while gpio.input(pir):
+		camera = PiCamera()
+		while (i<=2):
+			camera.capture('/home/pi/Desktop/image%s.jpg' % i)
+			print("Capturing...",i)
+			time.sleep(1)
+			i=i+1
+		camera.close()
+		i=0
+		time.sleep(10)
+
+#camera.start_recording('/home/pi/Desktop/video.h264')
+#sleep(5)
+#camera.stop_recording()
+
+
